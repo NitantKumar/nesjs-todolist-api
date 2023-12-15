30107f7715bb354d05572d14af740f42356ec65a
Rohan Shrivastava <rohanshrivastava10000@gmail.com>
2023-12-15 20:33:06 +0530

    commit 2

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const corsOptions: CorsOptions = {
-    origin: '*', // Allow all origins
+    origin: 'https://task-tracker-client.vercel.app/', 
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   };
